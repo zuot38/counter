c036b37bccc0c6c8e3d220be3bf313f1755e5d3a
zuot <128637031+zuot38@users.noreply.github.com>
2023-11-11 00:55:03 +0100

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,9 +1,13 @@
-let counter = 0;
+// Načtení aktuální hodnoty z localStorage
+let counter = localStorage.getItem("counter") || 0;
 
 // Funkce pro zvýšení počítadla a aktualizaci zobrazení
 function increaseCounter() {
     counter++;
     document.getElementById("counter").innerText = counter;
+
+    // Uložení hodnoty do localStorage
+    localStorage.setItem("counter", counter);
 }
 
 // Zavolání increaseCounter() při každé návštěvě stránky
